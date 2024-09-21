32ea5f87a27160fe4c54a1e3aaf43cacfa9f44d9
Christian Blackwell <christianblackwell@Christians-MacBook-Pro.local>
2024-09-21 14:44:10 -0500

    Updated index.js with updated port / import variables and package.json with clear start script

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from "express";
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
+const express = require("express");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
